2e494e079c80cec52de75373177f4fbbd3318405
Carlos Daniel Selva <cardansel@gmail.com>
2022-07-25 22:57:29 -0300

    Trabajando con Event binding(Enlace de eventos)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit, SimpleChange, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = '28day';
+export class AppComponent implements OnChanges, OnInit, OnDestroy {
+  cities = ['Argentina', 'EE.UU.', 'Brazil'];
+  name !: string;
+  titleWeb = '28day';
+  subTitle = 'Testing';
+  selection!: string;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Change->', changes);
+  }
+
+  ngOnInit(): void {
+    console.log('OnInit-> ');
+  }
+
+  ngOnDestroy(): void {
+    console.log('Destroy');
+  }
+
+  onCityClicked(city: string): void {
+    console.log('City->', city);
+    this.selection = city;
+  }
+
+  onClear(): void {
+    this.selection = '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
